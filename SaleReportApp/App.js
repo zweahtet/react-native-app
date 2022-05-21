3c424c54961b71f89d0a0ee5e24e1d615fa11aed
Zwea Htet <zhtet5@berkeley.edu>
2022-05-20 21:37:09 -0700

    Created Home and Form views and implemented project structure.

--- SaleReportApp/App.js
+++ SaleReportApp/App.js
@@ -1,5 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button} from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from './src/views/homescreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   const createSaleReport = () => {
@@ -7,16 +12,21 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Sale Report App</Text>
-      <Button
-        onPress={createSaleReport}
-        title="Create Sale Report"
-        color="#841584"
-        accessibilityLabel="Learn more about this purple button"
-      />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="AFC Sale Report" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    // <View style={styles.container}>
+    //   <Text style={styles.title}>Sale Report App</Text>
+    //   <Button
+    //     onPress={createSaleReport}
+    //     title="Create Sale Report"
+    //     color="#841584"
+    //     accessibilityLabel="Create Sale Report"
+    //   />
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
--- SaleReportApp/src/views/form/index.js
+++ SaleReportApp/src/views/form/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-export default function HomeScreen() {
+export default function Form() {
     return (
         <View>
-            <Text>Home</Text>
+            <Text>Form</Text>
         </View>
     )
 }
